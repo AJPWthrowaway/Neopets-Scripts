4f92b3e6c6c3d1707ffc57db040eed4fce48feec
AJPWthrowaway <151868512+AJPWthrowaway@users.noreply.github.com>
2023-11-23 11:12:15 -0700

    Update Buy 1000 Stocks.user.js

--- Buy 1000 Stocks.user.js
+++ Buy 1000 Stocks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Buy 1000 Stocks
 // @version      1.0.0
-// @description  Set stock shares amount to 1000
+// @description  Set stock shares amount to 1000 when buying
 // @author       AJPWthrowaway
 // @match        *://*.neopets.com/stockmarket.phtml?type=buy*
 // ==/UserScript==
